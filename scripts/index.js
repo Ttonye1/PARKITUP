839f6ae2920cfb20c471decd6e26ac33e46000b7
Victor Pepple <victorpepple043@gmail.com>
2024-10-08 21:22:31 -0700

    Completed the image slider and began the next part.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// header Javascript
+
 let searchBtn=document.querySelector(".searchBtn");
 let closeBtn=document.querySelector(".closeBtn");
 let searchbox=document.querySelector(".searchbox");
@@ -14,4 +16,42 @@ searchBtn.addEventListener('click', () => {
     searchBtn.classList.remove('active');
  });
 
-  
+// Image Slider Javascript
+let slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalid = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+   
+   if(slides.length > 0){
+      slides[slideIndex].classList.add("displaySlide");
+      intervalid=setInterval(nextSlide, 5000);
+   }
+};
+
+function showSlide(index){
+
+   if(index >= slides.length){
+      slideIndex = 0;}
+   else if(index < 0){
+      slideIndex = slides.length - 1;
+   }
+
+   slides.forEach(slide => {
+      slide.classList.remove("displaySlide");
+   });
+   slides[slideIndex].classList.add("displaySlide");
+};
+
+function prevSlide(){
+   clearInterval(intervalid);
+   slideIndex--;
+   showSlide(slideIndex);
+};
+
+function nextSlide(){
+   slideIndex++;
+   showSlide(slideIndex);
+};
